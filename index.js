0b5912ca2980e61a45d0b6b2d4207947c0b82a54
Glen Simpson <gsimpson@airmaster.com.au>
2018-07-04 00:00:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -152,7 +152,7 @@ function endGame() {
   clearInterval(gameInterval);
  
  for(let i = 0; i < 4; i++) {
-    document.remove(ROCKS[i]);
+    GAME.remove(ROCKS[i]);
   }
   
   document.querySelectorAll('.rock').forEach(function(a){
